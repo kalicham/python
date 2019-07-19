08929f730b728921c73ba7efe22bb5d4a4d1de17
kalicham <32410283+kalicham@users.noreply.github.com>
2019-07-19 15:17:33 +0600

    test slack

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8
+print('Введите количество минут: ')
+N = int(input())
+n = n % (24*60)
+h = n // 60
+m = n % 60
+print('Время: 'h,m)
+print('test')
